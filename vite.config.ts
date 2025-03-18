cf6f61e1a34c0501c47fdea33acf029dadc906c4
ssuzn <tnwls_723@naver.com>
2025-03-18 19:02:28 +0900

    chore: 프로젝트 초기 세팅

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '@assets',
+        replacement: path.resolve(__dirname, 'src/assets'),
+      },
+      {
+        find: '@pages',
+        replacement: path.resolve(__dirname, 'src/pages'),
+      },
+      {
+        find: '@store',
+        replacement: path.resolve(__dirname, 'src/store'),
+      },
+      {
+        find: '@styles',
+        replacement: path.resolve(__dirname, 'src/styles'),
+      },
+    ],
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
