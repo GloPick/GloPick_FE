d8edb1eee8feac876f4afe35bb075caf49508db7
ssuzn <tnwls_723@naver.com>
2025-04-01 17:53:05 +0900

    feat: header 컴포넌트 제작 (#7)

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,13 +2,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Test from '@/pages/test';
 import Landing from '@/pages/landing';
+import Layout from '@/components/layout';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/test" element={<Test />} />
-        <Route path="/" element={<Landing />} />
+        {/* 공통 layout 적용 */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="/test" element={<Test />} />
+          {/* <Route path="/home" element={<Home />} /> */}
+        </Route>
       </Routes>
     </BrowserRouter>
   );
